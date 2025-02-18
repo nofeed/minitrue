d8b2c9299ff97955185a806a2ae8c875a738a6c5
Nicholas Wieland (ngw) <nicholas@chorally.com>
2025-02-18 15:30:20 +0100

    Make ruff happy

--- tests/gpg_test.py
+++ tests/gpg_test.py
@@ -1,16 +1,14 @@
+from minitrue.gpg import KeyChain
 from tests.gpg_util import keys
-from minitrue.gpg import KeyChain, Key
-from itertools import count
 import re
-import pytest
 
 
-def test_keys(keys):
+def test_keys(keys: keys):
     keychain = KeyChain("")
     assert len(tuple(keychain.keylist)) == 2
 
 
-def test_key_fetch(keys):
+def test_key_fetch(keys: keys):
     email = "test2@example.com"
     keychain = KeyChain(email)
     email_rxp = re.compile(email)

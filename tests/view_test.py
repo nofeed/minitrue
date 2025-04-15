4d5a792404a5f0b972350e3e9fe7e1514cf20816
Nicholas Wieland (ngw) <nicholas@chorally.com>
2025-04-15 16:46:00 +0200

    Test for views

--- tests/view_test.py
+++ tests/view_test.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 from minitrue.view import View
 from minitrue.config import Config
@@ -6,7 +7,7 @@ from minitrue.config import Config
 
 config_path = Path(__file__).parent / "resources" / "config"
 
-def test_init(tmpdir):
+def test_init():
     path = Path(config_path)
     config = Config(str(path)).read()
     for source, destination in config.configs.items():
@@ -14,3 +15,12 @@ def test_init(tmpdir):
         assert view.source == source
         assert view.destination == destination
 
+
+def test_compile():
+    path = Path(config_path)
+    config = Config(str(path)).read()
+    for source, destination in config.configs.items():
+        os.remove(destination)
+        view = View(source, destination, config).compile()
+        assert Path(destination).is_file()
+ 
